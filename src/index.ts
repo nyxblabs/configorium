6a18fd917171ba8e25b196899ab23b676f051c42
nyxb <contact@nyxb.xyz>
2023-05-11 09:49:05 +0200

    ✨ feat(index.ts): add functionality to read, write, parse, and update configuration files ✅ test(index.test.ts): add tests for read, write, parse, and update functionality The index.ts file adds functionality to read, write, parse, and update configuration files. The read function reads a configuration file and returns its contents as an object. The write function writes an object to a configuration file. The parse function parses a configuration file and returns its contents as an object. The update function updates a configuration file with new values. The index.test.ts file contains tests for the read, write, parse, and update functionality.

---
+++ src/index.ts
@@ -0,0 +1,137 @@
+import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { homedir } from 'node:os'
+import destr from 'nyxjson'
+import flat from 'flat'
+import { nyxdefaults } from 'nyxdefaults'
+
+const RE_KEY_VAL = /^\s*([^\s=]+)\s*=\s*(.*)?\s*$/
+const RE_LINES = /\n|\r|\r\n/
+
+type RC = Record<string, any>
+
+interface RCOptions {
+   name?: string
+   dir?: string
+   flat?: boolean
+}
+
+export const defaults: RCOptions = {
+   name: '.conf',
+   dir: process.cwd(),
+   flat: false,
+}
+
+function withDefaults(options?: RCOptions | string): RCOptions {
+   if (typeof options === 'string')
+      options = { name: options }
+
+   return { ...defaults, ...options }
+}
+
+export function parse<T extends RC = RC>(
+   contents: string,
+   options: RCOptions = {},
+): T {
+   const config: RC = {}
+
+   const lines = contents.split(RE_LINES)
+
+   for (const line of lines) {
+      const match = line.match(RE_KEY_VAL)
+      if (!match)
+         continue
+
+      // Key
+      const key = match[1]
+
+      if (!key || key === '__proto__' || key === 'constructor')
+         continue
+
+      const value = destr(match[2].trim() /* val */)
+
+      if (key.endsWith('[]')) {
+         const nkey = key.slice(0, Math.max(0, key.length - 2))
+
+         config[nkey] = (config[nkey] || []).concat(value)
+         continue
+      }
+
+      config[key] = value
+   }
+
+   return options.flat
+      ? (config as T)
+      : flat.unflatten(config, { overwrite: true })
+}
+
+export function parseFile<T extends RC = RC>(
+   path: string,
+   options?: RCOptions,
+): T {
+   if (!existsSync(path))
+      return {} as T
+
+   return parse(readFileSync(path, 'utf8'), options)
+}
+
+export function read<T extends RC = RC>(options?: RCOptions | string): T {
+   options = withDefaults(options)
+   return parseFile(resolve(options.dir!, options.name!), options)
+}
+
+export function readUser<T extends RC = RC>(options?: RCOptions | string): T {
+   options = withDefaults(options)
+   options.dir = process.env.XDG_CONFIG_HOME || homedir()
+   return read(options)
+}
+
+export function serialize<T extends RC = RC>(config: T): string {
+   return Object.entries(flat.flatten<RC, RC>(config))
+      .map(
+         ([key, value]) =>
+        `${key}=${typeof value === 'string' ? value : JSON.stringify(value)}`,
+      )
+      .join('\n')
+}
+
+export function write<T extends RC = RC>(
+   config: T,
+   options?: RCOptions | string,
+) {
+   options = withDefaults(options)
+   writeFileSync(resolve(options.dir!, options.name!), serialize(config), {
+      encoding: 'utf8',
+   })
+}
+
+export function writeUser<T extends RC = RC>(
+   config: T,
+   options?: RCOptions | string,
+) {
+   options = withDefaults(options)
+   options.dir = process.env.XDG_CONFIG_HOME || homedir()
+   write(config, options)
+}
+
+export function update<T extends RC = RC>(
+   config: T,
+   options?: RCOptions | string,
+): T {
+   options = withDefaults(options)
+   if (!options.flat)
+      config = flat.unflatten(config, { overwrite: true })
+
+   const newConfig = nyxdefaults(config, read(options))
+   write(newConfig, options)
+   return newConfig as T
+}
+
+export function updateUser<T extends RC = RC>(
+   config: T,
+   options?: RCOptions | string,
+): T {
+   options = withDefaults(options)
+   options.dir = process.env.XDG_CONFIG_HOME || homedir()
+   return update(config, options)
+}
